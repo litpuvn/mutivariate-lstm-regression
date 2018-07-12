449e069c5cb31434636ce0cc437256e16b904617
Hoang Long <litpuvn@gmail.com>
2018-07-12 16:55:08 -0500

    add deep model

--- single_lag_input.py
+++ single_lag_input.py
@@ -8,8 +8,9 @@ from sklearn.preprocessing import MinMaxScaler
 from sklearn.preprocessing import LabelEncoder
 from sklearn.metrics import mean_squared_error
 from keras.models import Sequential
-from keras.layers import Dense
-from keras.layers import LSTM
+from keras.layers import Dense, Dropout, LSTM, Activation
+import keras.backend as K
+
 import numpy as np
 
 
@@ -86,11 +87,34 @@ train_X = train_X.reshape((train_X.shape[0], 1, train_X.shape[1]))
 test_X = test_X.reshape((test_X.shape[0], 1, test_X.shape[1]))
 print(train_X.shape, train_y.shape, test_X.shape, test_y.shape)
 
+
+def r2_keras(y_true, y_pred):
+    """Coefficient of Determination
+    """
+    SS_res =  K.sum(K.square( y_true - y_pred ))
+    SS_tot = K.sum(K.square( y_true - K.mean(y_true) ) )
+    return ( 1 - SS_res/(SS_tot + K.epsilon()) )
 # design network
+# model = Sequential()
+# model.add(LSTM(50, input_shape=(train_X.shape[1], train_X.shape[2])))
+# model.add(Dense(1))
+# model.compile(loss='mae', optimizer='adam')
+
+# deep network
 model = Sequential()
-model.add(LSTM(50, input_shape=(train_X.shape[1], train_X.shape[2])))
-model.add(Dense(1))
-model.compile(loss='mae', optimizer='adam')
+model.add(LSTM(units=100,
+               input_shape=(train_X.shape[1], train_X.shape[2]),
+               return_sequences=True
+               ))
+model.add(Dropout(0.1))
+model.add(LSTM(
+          units=50,
+          return_sequences=False))
+model.add(Dropout(0.1))
+model.add(Dense(units=1))
+model.add(Activation("linear"))
+model.compile(loss='mae', optimizer='adam', metrics=['mae', 'mean_squared_error', r2_keras])
+
 # fit network
 history = model.fit(train_X, train_y, epochs=50, batch_size=72, validation_data=(test_X, test_y), verbose=2,
                     shuffle=False)
