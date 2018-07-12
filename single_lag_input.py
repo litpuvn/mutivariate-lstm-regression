afee453c78a7aeff6ec530d6204129d7eb8e0558
Hoang Long <litpuvn@gmail.com>
2018-07-12 17:03:25 -0500

    ok

--- single_lag_input.py
+++ single_lag_input.py
@@ -94,6 +94,8 @@ def r2_keras(y_true, y_pred):
     SS_res =  K.sum(K.square( y_true - y_pred ))
     SS_tot = K.sum(K.square( y_true - K.mean(y_true) ) )
     return ( 1 - SS_res/(SS_tot + K.epsilon()) )
+
+
 # design network
 # model = Sequential()
 # model.add(LSTM(50, input_shape=(train_X.shape[1], train_X.shape[2])))
